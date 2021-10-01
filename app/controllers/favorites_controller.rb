cc62a072d6c4003fe25b13708b8cba36dbc758de
“takato15” <gshawks21@gmail.com>
2021-10-01 02:42:28 +0000

    [update]controller変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,15 +3,21 @@ class FavoritesController < ApplicationController
   def create
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
-    redirect_back(fallback_location: root_path)
+    # favorite.save
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
-    favorite.destroy
-    redirect_back(fallback_location: root_path)
+    # favorite.destroy
+    # redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def book_params
+    @book = Book.find(params[:id])
   end
 
 end
